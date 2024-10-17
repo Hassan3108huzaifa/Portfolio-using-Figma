64218ae19087903973787eac334c7e7f223dbc7e
Hassan <142681707+Hassan3108huzaifa@users.noreply.github.com>
2024-10-17 12:06:41 -0700

    layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,40 +4,38 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
-// Load your local fonts
 const manRope = localFont({
-  src: "./fonts/manrope.ttf", // Manrope font file
+  src: "./fonts/manrope.ttf",
   variable: "--font-manrope",
   weight: "100 900",
 });
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff", // Geist Sans font file
+  src: "./fonts/GeistVF.woff", 
   variable: "--font-geist-sans",
   weight: "100 900",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff", // Geist Mono font file
+  src: "./fonts/GeistMonoVF.woff", 
   variable: "--font-geist-mono",
   weight: "100 900",
 });
 const inter = localFont({
-  src: "./fonts/Inter.ttf", // Ensure you have the correct Inter font file
+  src: "./fonts/Inter.ttf",
   variable: "--font-inter",
   weight: "100 900",
 });
 
 const bebasNeue = localFont({
-  src: "./fonts/BebasNeue-Regular.ttf", // Make sure this path is correct
+  src: "./fonts/BebasNeue-Regular.ttf",
   variable: "--font-bebas-neue",
 });
 
-// Metadata for the page
+
 export const metadata: Metadata = {
   title: "Portfolio - Assigment",
   description: "Created portfolio website using figma",
 };
 
-// Root Layout Component
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -48,7 +46,7 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        {/* Google Fonts can be included here if needed */}
+      
       </head>
       <body
         className={`${manRope.variable} ${geistSans.variable} ${geistMono.variable} ${inter.variable} ${bebasNeue.variable} antialiased`}
